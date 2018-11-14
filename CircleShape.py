f6d9cfc90b110e038799bff0a8af25d72ad65317
Rustam <rystamrv@gmail.com>
2018-11-14 21:35:20 +0200

    Update CircleShape.py

--- CircleShape.py
+++ CircleShape.py
@@ -2,10 +2,17 @@ from LogicalPoint import *
 
 class CircleShape(LogicalPoint):
 
-    def __init__(self, x, y, r, color):
+    def __init__(self, x, y, r, color, isBallet):
         super().__init__(x, y)
         self.radius = r
         self.color = color
+        self.__isBallet = isBallet
 
     def draw(self, p, window):
         p.draw.circle(window, self.color, (self.getX(), self.getY()), self.radius)
+
+    def getIsBallet(self):
+        return self.__isBallet
+
+    def setIsBallet(self, isBallet):
+        self.__isBallet = isBallet
