bfefa847b1f24c60eeb1fc311d71a0de95b693e1
Rustam <rystamrv@gmail.com>
2018-11-14 21:36:50 +0200

    Update Player.py

--- Player.py
+++ Player.py
@@ -2,12 +2,14 @@ from LogicalPoint import *
 
 class Player(LogicalPoint):
 
-    def __init__(self, x, y, height, width, color):
+    def __init__(self, x, y, height, width, color, isCreate, side):
         super().__init__(x, y)
         self.__height = height
         self.__width = width
         self.__health = 100
         self.__color = color
+        self.__isCreate = isCreate
+        self.__side = side
 
     def getHeight(self):
         return self.__height
@@ -30,6 +32,18 @@ class Player(LogicalPoint):
     def wound(self, wound):
         self.__health -= wound
 
+    def getCreate(self):
+        return self.__isCreate
+
+    def setCreate(self, isCreate):
+        self.__isCreate = isCreate
+
+    def setSide(self, side):
+        if side is not None:
+            self.__side = side
+
+    def getSide(self):
+        return self.__side
 
     def draw(self, p, w):
         p.draw.rect(w, self.getColor(), (self.getX(), self.getY(), self.getHeight(), self.getWidth()))
