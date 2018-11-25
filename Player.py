f0b63cb5ffd47de704baf6fe74fd69a0f0ed8820
Rustam <rystamrv@gmail.com>
2018-11-25 16:41:02 +0200

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,42 +1,61 @@
-from LogicalPoint import *
+from LogicalRect import *
 
-class Player(LogicalPoint):
 
-    def __init__(self, x, y, height, width, color, isCreate, side):
-        super().__init__(x, y)
+class Player:
+    """
+    class Player
+    """
+    def __init__(self, x, y, height, width, color, side):
+        """
+        :param x: point x
+        :param y: point y
+        :param height: player height
+        :param width: player width
+        :param color: player color
+        :param side:
+        """
         self.__height = height
         self.__width = width
         self.__health = 100
         self.__color = color
-        self.__isCreate = isCreate
         self.__side = side
-
-    def getHeight(self):
-        return self.__height
-
-    def getWidth(self):
-        return self.__width
+        self.__shape = LogicalRect(x, y, height, width, color)
 
     def setColor(self, color):
+        """
+        set color
+        :param color: accepts player color
+        """
         self.__color = color
 
     def getColor(self):
+        """ :return: color  """
         return self.__color
 
     def getHealth(self):
+        """ :return: health """
         return self.__health
 
     def setHealth(self, health):
+        """
+        set health
+        :param health: accepts player health
+        """
         self.__health = health
 
     def wound(self, wound):
+        """
+        wounded player
+        :param wound: number of wounds
+        """
         self.__health -= wound
 
-    def getCreate(self):
-        return self.__isCreate
-
-    def setCreate(self, isCreate):
-        self.__isCreate = isCreate
+    def isDead(self):
+        """
+        check is dead
+        :return: true if player dead
+        """
+        return self.getHealth() < 0
 
     def setSide(self, side):
         if side is not None:
@@ -45,7 +64,16 @@ class Player(LogicalPoint):
     def getSide(self):
         return self.__side
 
+    def getShape(self):
+        """
+        :return: shape
+        """
+        return self.__shape
+
     def draw(self, p, w):
-        p.draw.rect(w, self.getColor(), (self.getX(), self.getY(), self.getHeight(), self.getWidth()))
-
-
+        """
+        draw player
+        :param p: pygame
+        :param w: window
+        """
+        self.__shape.draw(p, w)
