dadfe1643819a266712b3fe442e750de0764eb9b
Rustam <rystamrv@gmail.com>
2018-11-27 10:37:37 +0200

    Update WorkFile.py

--- WorkFile.py
+++ WorkFile.py
@@ -3,8 +3,7 @@ import json
 class WorkFile:
 
     def __init__(self, file):
-        self.file = file
-        self.list = None
+        self.file = file 
 
     def read(self):
         file = open(self.file, mode="r")
